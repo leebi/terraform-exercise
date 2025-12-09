037953fb641029e505c9e064cb5a7850eecaf1bc
Matthias <x@leebis.net>
2025-12-09 13:18:25 +0100

    set remote state file

--- main.tf
+++ main.tf
@@ -1,9 +1,15 @@
 terraform {
-  required_version = ">=1.7.0"
-  required_providers {
-    aws = {
-        source = "hashicorp/aws"
-        version = ">=5.0"
+    required_version = ">=1.7.0"
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = ">=5.0"
+        }
     }
-  }
+    backend "s3" {
+        bucket         = "vertice-exercise"
+        key            = "terraform-exercise/terraform.tfstate"
+        region         = "eu-west-1"
+        encrypt        = true
+    }
 }
